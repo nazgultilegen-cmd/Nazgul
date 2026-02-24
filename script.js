9bec174b4b928cd1ff0582d9777e05ae38839f1d
Nazgul T <nazgul.tilegen@invisionu.education>
2026-02-24 20:40:03 +0500

    BMI Calculator

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
-// Starter JS
-// Steps for students to implement:
-// 1. Get user inputs
-// 2. Perform calculations
-// 3. Use if/else to classify results
-// 4. Display results in #output
+function calculate() {
+  var name= document.getElementById("name").value;
+  var weight= document.getElementById("weight").value;
+  var height= document.getElementById("height").value;
+
+  var heightMeters= height/100;
+  var bmi = weight/ (heightMeters*heightMeters);
+
+  bmi= bmi.toFixed(1);
+
+  var category;
+
+  if (bmi <=15.9) {
+    category = "Very Severely Underweight";
+  }
+  else if (bmi <= 16.9) {
+    category = "Severely Underweight";
+  }
+  else if (bmi <= 18.4) {
+    category = "Underweight";
+  }
+  else if (bmi <= 24.9) {
+    category = "Normal";
+  }
+  else if (bmi <= 29.9) {
+    category = "Overweight";
+  }
+  else if (bmi <= 34.9) {
+    category = "Obese Class 1";
+  }
+  else if (bmi <= 34.9) {
+    category = "Obese Class 1";
+  }
+  else if (bmi <= 39.9) {
+    category = "Obese Class 2";
+  }
+  else {
+    category = "Obese Class 3";
+  }
+
+document.getElementById("output").innerHTML=
+  "Hi,"+ name + "!</br>" +
+  "Your BMI is:"+bmi+"<br>"+
+  "Category:"+category;
+}
