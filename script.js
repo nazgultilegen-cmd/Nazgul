28b3f67de4dff80cb8a44976d0c42bafc3a769e0
Nazgul T <nazgul.tilegen@invisionu.education>
2026-03-13 15:56:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,56 @@
 function calculate() {
-  var name= document.getElementById("name").value;
-  var weight= document.getElementById("weight").value;
-  var height= document.getElementById("height").value;
 
-  var heightMeters= height/100;
-  var bmi = weight/ (heightMeters*heightMeters);
+    
+    var name = document.getElementById("name").value;
+    var weight = document.getElementById("weight").value;
+    var height = document.getElementById("height").value;
+    if (name === "" || weight <= 0 || height <= 0) {
+        document.getElementById("output").innerHTML = "Please enter valid data.";
+        return;
+    }
 
-  bmi= bmi.toFixed(1);
+    
+    var heightMeters = height / 100;
+    var bmi = weight / (heightMeters * heightMeters);
 
-  var category;
+    
+    bmi = bmi.toFixed(1);
 
-  if (bmi <=15.9) {
-    category = "Very Severely Underweight";
-  }
-  else if (bmi <= 16.9) {
-    category = "Severely Underweight";
-  }
-  else if (bmi <= 18.4) {
-    category = "Underweight";
-  }
-  else if (bmi <= 24.9) {
-    category = "Normal";
-  }
-  else if (bmi <= 29.9) {
-    category = "Overweight";
-  }
-  else if (bmi <= 34.9) {
-    category = "Obese Class 1";
-  }
-  else if (bmi <= 39.9) {
-    category = "Obese Class 2";
-  }
-  else {
-    category = "Obese Class 3";
-  }
+    var category;
 
-document.getElementById("output").innerHTML=
-  "Hi,"+ name + "!</br>" +
-  "Your BMI is:"+bmi+"<br>"+
-  "Category:"+category;
+    
+
+    if (bmi <= 15.9) {
+        category = "Very Severely Underweight";
+    } 
+    else if (bmi <= 16.9) {
+        category = "Severely Underweight";
+    } 
+    else if (bmi <= 18.4) {
+        category = "Underweight";
+    } 
+    else if (bmi <= 24.9) {
+        category = "Normal";
+    } 
+    else if (bmi <= 29.9) {
+        category = "Overweight";
+    } 
+    else if (bmi <= 34.9) {
+        category = "Obese Class I";
+    } 
+    else if (bmi <= 39.9) {
+        category = "Obese Class II";
+    } 
+    else if ( bmi <= 99.9) {
+        category = "Obese Class III";
+    }
+    else if (bmi => 99.9) {
+        category = "you are lier it can be real";
+    } 
+
+    
+    document.getElementById("output").innerHTML =
+        "Hi, " + name + "!<br>" +
+        "Your BMI is: " + bmi + "<br>" +
+        "Category: " + category;
 }
